835c34e04965c390cc4a4c35494a84c340d24cb0
Ryo Suzuki <reputeless+github@gmail.com>
2020-03-01 23:41:03 +0900

    Console (mac)

---
+++ Siv3D/src/Siv3D-Platform/macOS/Console/CConsole.cpp
@@ -0,0 +1,36 @@
+//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2020 Ryo Suzuki
+//	Copyright (c) 2016-2020 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include <Common/ApplicationOptions.hpp>
+# include "CConsole.hpp"
+
+namespace s3d
+{
+	CConsole::CConsole()
+	{
+		// do nothing
+	}
+
+	CConsole::~CConsole()
+	{
+		// do nothing
+	}
+
+	void CConsole::open()
+	{
+		// do nothing
+	}
+
+	void CConsole::close()
+	{
+		// do nothing
+	}
+}
