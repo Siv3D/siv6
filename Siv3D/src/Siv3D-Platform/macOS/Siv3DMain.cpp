b94c886b0d368e775698bcb110569cd89bd358e6
Ryo Suzuki <reputeless+github@gmail.com>
2020-03-01 17:14:04 +0900

    Xcode プロジェクト追加

---
+++ Siv3D/src/Siv3D-Platform/macOS/Siv3DMain.cpp
@@ -0,0 +1,23 @@
+//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2020 Ryo Suzuki
+//	Copyright (c) 2016-2020 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include <iostream>
+
+void Main();
+
+int main(int, char*[])
+{
+	std::cout << "OpenSiv3D for macOS\n";
+	
+	Main();
+
+	return 0;
+}
---
+++ macOS/Main.cpp
@@ -0,0 +1,7 @@
+
+# include <Siv3D.hpp> // OpenSiv3D v0.4.3
+
+void Main()
+{
+
+}
