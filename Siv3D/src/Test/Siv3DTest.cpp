eb457b39b753fa4979c3b5b31c250013a68938eb
Ryo Suzuki <reputeless+github@gmail.com>
2020-03-03 17:33:48 +0900

    [共通] Add Siv3DTest

---
+++ Siv3D/src/Test/Siv3DTest.cpp
@@ -0,0 +1,34 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2020 Ryo Suzuki
+//	Copyright (c) 2016-2020 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include <Siv3D.hpp>
+# define CATCH_CONFIG_RUNNER
+# define CATCH_CONFIG_FAST_COMPILE
+# include <Catch2/catch.hpp>
+
+# if SIV3D_PLATFORM(WINDOWS)
+#	include <conio.h>
+# endif
+
+using namespace s3d;
+
+void PerformTest()
+{
+	Console.open();
+
+	Catch::Session().run();
+
+# if SIV3D_PLATFORM(WINDOWS)
+
+	_getch();
+
+# endif
+}
