42d7e46bbc340c45a61adc48ea9264c99b028bec
Ryo Suzuki <reputeless+github@gmail.com>
2020-03-03 18:52:08 +0900

    [共通] Update Test

--- Siv3D/src/Test/Siv3DTest.cpp
+++ Siv3D/src/Test/Siv3DTest.cpp
@@ -10,13 +10,15 @@
 //-----------------------------------------------
 
 # include <Siv3D.hpp>
-# define CATCH_CONFIG_RUNNER
-# define CATCH_CONFIG_FAST_COMPILE
-# include <Catch2/catch.hpp>
 
 # if SIV3D_PLATFORM(WINDOWS)
 #	include <conio.h>
+#	pragma warning(disable : 6237 6319 26439 26451 26495 26812)
 # endif
+
+# define CATCH_CONFIG_RUNNER
+# define CATCH_CONFIG_FAST_COMPILE
+# include <Catch2/catch.hpp>
 
 using namespace s3d;
 
@@ -28,7 +30,49 @@ void PerformTest()
 
 # if SIV3D_PLATFORM(WINDOWS)
 
-	_getch();
+	(void)_getch();
 
 # endif
+}
+
+TEST_CASE("Unicode")
+{
+	SECTION("Narrow")
+	{
+		REQUIRE(Unicode::Narrow(U"") == "");
+		REQUIRE(Unicode::Narrow(U"OpenSiv3D") == "OpenSiv3D");
+		REQUIRE(Unicode::Narrow(U"あいうえお") == "あいうえお");
+	}
+
+	SECTION("ToWstring")
+	{
+		REQUIRE(Unicode::ToWstring(U"") == L"");
+		REQUIRE(Unicode::ToWstring(U"OpenSiv3D") == L"OpenSiv3D");
+		REQUIRE(Unicode::ToWstring(U"あいうえお") == L"あいうえお");
+	}
+
+	SECTION("ToUTF8")
+	{
+		const std::u8string_view u0 = u8"OpenSiv3D";
+		const std::u8string_view u1 = u8"あいうえお";
+		const std::string s0(u0.begin(), u0.end());
+		const std::string s1(u1.begin(), u1.end());
+		REQUIRE(Unicode::ToUTF8(U"") == "");
+		REQUIRE(Unicode::ToUTF8(U"OpenSiv3D") == s0);
+		REQUIRE(Unicode::ToUTF8(U"あいうえお") == s1);
+	}
+
+	SECTION("ToUTF16")
+	{
+		REQUIRE(Unicode::ToUTF16(U"") == u"");
+		REQUIRE(Unicode::ToUTF16(U"OpenSiv3D") == u"OpenSiv3D");
+		REQUIRE(Unicode::ToUTF16(U"あいうえお") == u"あいうえお");
+	}
+
+	SECTION("ToUTF32")
+	{
+		REQUIRE(Unicode::ToUTF32(U"") == U"");
+		REQUIRE(Unicode::ToUTF32(U"OpenSiv3D") == U"OpenSiv3D");
+		REQUIRE(Unicode::ToUTF32(U"あいうえお") == U"あいうえお");
+	}
 }
