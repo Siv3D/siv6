3eb1030a306a355857bfdf6a42d42e47576d51d1
Ryo Suzuki <reputeless+github@gmail.com>
2020-03-03 14:23:19 +0900

    [共通] UnicodeUtility を追加

---
+++ Siv3D/src/Siv3D/Unicode/UnicodeUtility.cpp
@@ -0,0 +1,96 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2020 Ryo Suzuki
+//	Copyright (c) 2016-2020 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include "UnicodeUtility.hpp"
+
+namespace s3d
+{
+	namespace detail
+	{
+		size_t UTF8_Length(const char32 codePoint) noexcept
+		{
+			if (codePoint < 0x80) // 0x00 - 0x7F
+			{
+				return 1;
+			}
+			else if (codePoint < 0x800) // 0x80 - 0x07FF
+			{
+				// [Siv3D ToDo] 不正なコードポイントをはじく
+				return 2;
+			}
+			else if (codePoint < 0x10000) // 0x0800 - 0xFFFF
+			{
+				// [Siv3D ToDo] 不正なコードポイントをはじく
+				return 3;
+			}
+			else if (codePoint < 0x110000) // 0x010000 - 0x10FFFF
+			{
+				// [Siv3D ToDo] 不正なコードポイントをはじく
+				return 4;
+			}
+			else // Invalid code point
+			{
+				return 3; // REPLACEMENT CHARACTER (0xEF 0xBF 0xBD)
+			}
+		}
+
+		size_t UTF8_Length(const StringView s) noexcept
+		{
+			size_t result = 0;
+
+			const char32* pSrc = s.data();
+			const char32* const pSrcEnd = pSrc + s.size();
+
+			while (pSrc != pSrcEnd)
+			{
+				result += UTF8_Length(*pSrc++);
+			}
+
+			return result;
+		}
+
+		void UTF8_Encode(char8** s, const char32 codePoint) noexcept
+		{
+			if (codePoint < 0x80) // 0x00 - 0x7F
+			{
+				*(*s)++ = static_cast<char8>(codePoint);
+			}
+			else if (codePoint < 0x800) // 0x80 - 0x07FF
+			{
+				// [Siv3D ToDo] 不正なコードポイントをはじく
+				*(*s)++ = (0xC0 | static_cast<char8>(codePoint >> 6));
+				*(*s)++ = (0x80 | static_cast<char8>(codePoint & 0x3F));
+			}
+			else if (codePoint < 0x10000) // 0x0800 - 0xFFFF
+			{
+				// [Siv3D ToDo] 不正なコードポイントをはじく
+				*(*s)++ = (0xE0 | static_cast<char8>(codePoint >> 12));
+				*(*s)++ = (0x80 | static_cast<char8>((codePoint >> 6) & 0x3F));
+				*(*s)++ = (0x80 | static_cast<char8>(codePoint & 0x3F));
+			}
+			else if (codePoint < 0x110000) // 0x010000 - 0x10FFFF
+			{
+				// [Siv3D ToDo] 不正なコードポイントをはじく
+				*(*s)++ = (0xF0 | static_cast<char8>(codePoint >> 18));
+				*(*s)++ = (0x80 | static_cast<char8>((codePoint >> 12) & 0x3F));
+				*(*s)++ = (0x80 | static_cast<char8>((codePoint >> 6) & 0x3F));
+				*(*s)++ = (0x80 | static_cast<char8>(codePoint & 0x3F));
+			}
+			else // Invalid code point
+			{
+				// REPLACEMENT CHARACTER (0xEF 0xBF 0xBD)
+				*(*s)++ = static_cast<char8>(uint8(0xEF));
+				*(*s)++ = static_cast<char8>(uint8(0xBF));
+				*(*s)++ = static_cast<char8>(uint8(0xBD));
+			}
+		}
+	}
+}
---
+++ Siv3D/src/Siv3D/Unicode/UnicodeUtility.hpp
@@ -0,0 +1,27 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2020 Ryo Suzuki
+//	Copyright (c) 2016-2020 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# pragma once
+# include <Siv3D/String.hpp>
+
+namespace s3d
+{
+	namespace detail
+	{
+		[[nodiscard]]
+		size_t UTF8_Length(const char32 codePoint) noexcept;
+
+		[[nodiscard]]
+		size_t UTF8_Length(const StringView s) noexcept;
+
+		void UTF8_Encode(char8** s, const char32 codePoint) noexcept;
+	}
+}
