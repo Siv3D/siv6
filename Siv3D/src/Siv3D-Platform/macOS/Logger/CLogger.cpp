708cca197f81c7fc3df3d12b7a666647a002634b
Ryo Suzuki <reputeless+github@gmail.com>
2020-03-21 19:25:24 +0900

    [macOS] Logger

---
+++ Siv3D/src/Siv3D-Platform/macOS/Logger/CLogger.cpp
@@ -0,0 +1,41 @@
+//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2020 Ryo Suzuki
+//	Copyright (c) 2016-2020 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include <Siv3D/String.hpp>
+# include <Siv3D/FormatLiteral.hpp>
+# include "CLogger.hpp"
+
+namespace s3d
+{
+	CLogger::CLogger()
+	{
+
+	}
+
+	CLogger::~CLogger()
+	{
+	
+	}
+
+	void CLogger::write(const LogType type, const StringView s)
+	{
+		//const int64 timeStamp = Time::GetMillisec() - g_applicationTime;
+
+		type;
+
+		const std::string output = U"{}\n"_fmt(s).narrow();
+
+		std::lock_guard lock(m_mutex);
+		{
+			std::cout << output;
+		}
+	}
+}
