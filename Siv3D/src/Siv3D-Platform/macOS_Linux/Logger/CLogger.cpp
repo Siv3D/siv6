cb68263fb9b8dc1db3ae0509156a7a4d6405f03e
Ryo Suzuki <reputeless+github@gmail.com>
2020-04-18 00:39:55 +0900

    [macOS] Update

--- Siv3D/src/Siv3D-Platform/macOS_Linux/Logger/CLogger.cpp
+++ Siv3D/src/Siv3D-Platform/macOS_Linux/Logger/CLogger.cpp
@@ -9,6 +9,7 @@
 //
 //-----------------------------------------------
 
+# include <array>
 # include <Siv3D/String.hpp>
 # include <Siv3D/FormatLiteral.hpp>
 # include <Siv3D/Time.hpp>
@@ -16,6 +17,20 @@
 
 namespace s3d
 {
+	namespace detail
+	{
+		constexpr std::array<StringView, 7> LogTypeNames =
+		{
+			U"[error]   "_sv,
+			U"[fail]    "_sv,
+			U"[warning] "_sv,
+			U""_sv,
+			U"[info]    "_sv,
+			U"[trace]   "_sv,
+			U"[verbose] "_sv,
+		};
+	}
+
 	const int64 g_applicationTime = Time::GetMillisec();
 
 	CLogger::CLogger()
@@ -31,10 +46,8 @@ namespace s3d
 	void CLogger::write(const LogType type, const StringView s)
 	{
 		const int64 timeStamp = Time::GetMillisec() - g_applicationTime;
-
-		type;
-
-		const std::string output = U"{}: {}\n"_fmt(timeStamp, s).narrow();
+		const StringView logTypeName = detail::LogTypeNames[FromEnum(type)];
+		const std::string output = U"{}: {}{}\n"_fmt(timeStamp, logTypeName, s).narrow();
 
 		std::lock_guard lock(m_mutex);
 		{
