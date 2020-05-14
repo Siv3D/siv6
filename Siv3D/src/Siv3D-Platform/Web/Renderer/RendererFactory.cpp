d774a67b716c97527a912b5db4324337fc68de93
nokotan <kamenokonokotan@gmail.com>
2020-05-14 23:13:47 +0900

    [Web] Copy source files from Linux version

---
+++ Siv3D/src/Siv3D-Platform/Web/Renderer/RendererFactory.cpp
@@ -0,0 +1,20 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2020 Ryo Suzuki
+//	Copyright (c) 2016-2020 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include "GLES3/CRenderer_GLES3.hpp"
+
+namespace s3d
+{
+	ISiv3DRenderer* ISiv3DRenderer::Create()
+	{
+		return new CRenderer_GLES3;
+	}
+}
