18842d62c2d54ed5be670ceb0e9ea48242831573
nokotan <kamenokonokotan@gmail.com>
2020-05-24 00:36:53 +0900

    [Linux] Revert include order (glew.h, glfw3.h) in CRenderer_GLES3.hpp

--- Siv3D/src/Siv3D-Platform/Linux/Renderer/GL4/CRenderer_GL4.hpp
+++ Siv3D/src/Siv3D-Platform/Linux/Renderer/GL4/CRenderer_GL4.hpp
@@ -10,10 +10,10 @@
 //-----------------------------------------------
 
 # pragma once
-# include <Siv3D/Common.hpp>
-# include <Siv3D/Renderer/IRenderer.hpp>
 # include <GL/glew.h>
 # include <GLFW/glfw3.h>
+# include <Siv3D/Common.hpp>
+# include <Siv3D/Renderer/IRenderer.hpp>
 
 namespace s3d
 {
