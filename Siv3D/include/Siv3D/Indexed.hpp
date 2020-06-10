2e1c0ed46d63bd581d3554de9ceb730d08fa1d80
Ryo Suzuki <reputeless+github@gmail.com>
2020-06-10 19:09:59 +0900

    [共通] 整理

--- Siv3D/include/Siv3D/Indexed.hpp
+++ Siv3D/include/Siv3D/Indexed.hpp
@@ -19,63 +19,19 @@ namespace s3d
 {
 	template <class Type, class It = decltype(std::begin(std::declval<Type>())), class = decltype(std::end(std::declval<Type>()))>
 	[[nodiscard]]
-	inline constexpr auto IndexedRef(Type&& iterable)
-	{
-		// Reference: http://reedbeta.com/blog/python-like-enumerate-in-cpp17/
-
-		struct Iterator
-		{
-			size_t index;
-			It it;
-			constexpr bool operator != (const Iterator& other) const { return it != other.it; }
-			constexpr void operator ++() { ++index; ++it; }
-			constexpr auto operator *() const { return std::tie(index, *it); }
-		};
-
-		struct IterableWrapper
-		{
-			Type iterable;
-			constexpr auto begin() { return Iterator{ 0, std::begin(iterable) }; }
-			constexpr auto end() { return Iterator{ 0, std::end(iterable) }; }
-		};
-
-		return IterableWrapper{ std::forward<Type>(iterable) };
-	}
+	inline constexpr auto IndexedRef(Type&& iterable);
 
 	template <class Type, class It = decltype(std::cbegin(std::declval<Type>())), class = decltype(std::cend(std::declval<Type>()))>
 	[[nodiscard]]
-	inline constexpr auto Indexed(Type&& iterable)
-	{
-		return IndexedRef<Type, It>(std::forward<Type>(iterable));
-	}
+	inline constexpr auto Indexed(Type&& iterable);
 
 	template <class Type, class It = decltype(std::rbegin(std::declval<Type>())), class = decltype(std::rend(std::declval<Type>()))>
 	[[nodiscard]]
-	inline constexpr auto ReverseIndexedRef(Type&& iterable)
-	{
-		struct Iterator
-		{
-			size_t index;
-			It it;
-			constexpr bool operator != (const Iterator& other) const { return it != other.it; }
-			constexpr void operator ++() { --index; ++it; }
-			constexpr auto operator *() const { return std::tie(index, *it); }
-		};
-
-		struct IterableWrapper
-		{
-			Type iterable;
-			constexpr auto begin() { return Iterator{ std::size(iterable) - 1, std::rbegin(iterable) }; }
-			constexpr auto end() { return Iterator{ 0, std::rend(iterable) }; }
-		};
-
-		return IterableWrapper{ std::forward<Type>(iterable) };
-	}
+	inline constexpr auto ReverseIndexedRef(Type&& iterable);
 
 	template <class Type, class It = decltype(std::crbegin(std::declval<Type>())), class = decltype(std::crend(std::declval<Type>()))>
 	[[nodiscard]]
-	inline constexpr auto ReverseIndexed(Type&& iterable)
-	{
-		return ReverseIndexedRef<Type, It>(std::forward<Type>(iterable));
-	}
+	inline constexpr auto ReverseIndexed(Type&& iterable);
 }
+
+# include "detail/Indexed.ipp"
