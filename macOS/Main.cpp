a4f95d1b8d081fe4d3425d0e35e591b3ffbdbc82
Ryo Suzuki <reputeless+github@gmail.com>
2020-06-20 21:55:45 +0900

    [macOS] update

--- macOS/Main.cpp
+++ macOS/Main.cpp
@@ -4,8 +4,12 @@
 SIV3D_SET(EngineOption::StdErr,		true)
 SIV3D_SET(EngineOption::Renderer,	RendererType::OpenGL)
 
+void Siv3DTest();
+
 void Main()
 {
+	Siv3DTest();
+	
 	//Cursor::Register(U"MyCursor", Image(48, 48, Arg::generator0_1 = Gen), Point(24, 24));
 
 	while (System::Update())
