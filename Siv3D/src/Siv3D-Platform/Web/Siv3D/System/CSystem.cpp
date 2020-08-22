f22ff1f6e2215a559d380ee22b5af681034ca3c0
nokotan <kamenokonokotan@gmail.com>
2020-08-22 20:58:27 +0900

    [Web] CSystem の更新

--- Siv3D/src/Siv3D-Platform/Web/Siv3D/System/CSystem.cpp
+++ Siv3D/src/Siv3D-Platform/Web/Siv3D/System/CSystem.cpp
@@ -11,11 +11,19 @@
 
 # include <Siv3D/EngineLog.hpp>
 # include <Siv3D/Common/Siv3DEngine.hpp>
+# include <Siv3D/Resource/IResource.hpp>
 # include <Siv3D/Profiler/IProfiler.hpp>
+# include <Siv3D/LicenseManager/ILicenseManager.hpp>
+# include <Siv3D/ImageDecoder/IImageDecoder.hpp>
+# include <Siv3D/ImageEncoder/IImageEncoder.hpp>
 # include <Siv3D/Window/IWindow.hpp>
 # include <Siv3D/Scene/IScene.hpp>
+# include <Siv3D/Cursor/ICursor.hpp>
 # include <Siv3D/Renderer/IRenderer.hpp>
+# include <Siv3D/Renderer2D/IRenderer2D.hpp>
 # include <Siv3D/UserAction/IUserAction.hpp>
+# include <Siv3D/System/SystemLog.hpp>
+# include <Siv3D/System/SystemMisc.hpp>
 # include "CSystem.hpp"
 
 namespace s3d
@@ -27,26 +35,27 @@ namespace s3d
 
 	CSystem::~CSystem()
 	{
+		LOG_SCOPED_TRACE(U"CSystem::~CSystem()");
 
+		SystemMisc::Destroy();
+		SystemLog::Final();
 	}
 
 	void CSystem::init()
 	{
+		LOG_SCOPED_TRACE(U"CSystem::init()");
+		
+		SystemLog::Initial();
+		SystemMisc::Init();
+		
+		SIV3D_ENGINE(Resource)->init();
 		SIV3D_ENGINE(Profiler)->init();
 		SIV3D_ENGINE(Window)->init();
+		SIV3D_ENGINE(ImageDecoder)->init();
+		SIV3D_ENGINE(ImageEncoder)->init();
+		SIV3D_ENGINE(Cursor)->init();
 		SIV3D_ENGINE(Renderer)->init();
-	}
-
-	void CSystem::onMainThreadStart()
-	{
-		LOG_SCOPED_TRACE(U"CSystem::onMainThreadStart()");
-
-		SIV3D_ENGINE(Renderer)->onMainThreadStart();
-	}
-
-	void CSystem::onMainThreadTerminate()
-	{
-		LOG_SCOPED_TRACE(U"CSystem::onMainThreadTerminate()");
+		SIV3D_ENGINE(Renderer2D)->init();
 	}
 
 	bool CSystem::update()
@@ -75,6 +84,10 @@ namespace s3d
 		SIV3D_ENGINE(Scene)->update();
 		SIV3D_ENGINE(Window)->update();
 		SIV3D_ENGINE(Renderer)->clear();
+		SIV3D_ENGINE(Cursor)->update();
+
+		// triggerd by key inputs
+		SIV3D_ENGINE(LicenseManager)->update();
 		
 		return true;
 	}
