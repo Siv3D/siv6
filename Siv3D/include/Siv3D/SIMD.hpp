e8ddb5e9dc1ff8754528a3e42b3713d212f2820e
nokotan <kamenokonokotan@gmail.com>
2020-08-22 20:19:05 +0900

    [共通] simde の適用条件を変更

--- Siv3D/include/Siv3D/SIMD.hpp
+++ Siv3D/include/Siv3D/SIMD.hpp
@@ -14,43 +14,43 @@
 
 # define SIMDE_ENABLE_NATIVE_ALIASES
 
-# if __has_include(<xmmintrin.h>)
+# if __has_include(<xmmintrin.h>) && !SIV3D_PLATFORM(WEB)
 # include <xmmintrin.h>
 # else
 # include <ThirdParty/simde/x86/sse.h>
 # endif
 
-# if __has_include(<xmmintrin.h>)
+# if __has_include(<xmmintrin.h>) && !SIV3D_PLATFORM(WEB)
 # include <xmmintrin.h>
 # else
 # include <ThirdParty/simde/x86/sse.h>
 # endif
 
-# if __has_include(<emmintrin.h>)
+# if __has_include(<emmintrin.h>) && !SIV3D_PLATFORM(WEB)
 # include <emmintrin.h>
 # else
 # include <ThirdParty/simde/x86/sse2.h>
 # endif
 
-# if __has_include(<pmmintrin.h>)
+# if __has_include(<pmmintrin.h>) && !SIV3D_PLATFORM(WEB)
 # include <pmmintrin.h>
 # else
 # include <ThirdParty/simde/x86/sse3.h>
 # endif
 
-# if __has_include(<tmmintrin.h>)
+# if __has_include(<tmmintrin.h>) && !SIV3D_PLATFORM(WEB)
 # include <tmmintrin.h>
 # else
 # include <ThirdParty/simde/x86/ssse3.h>
 # endif
 
-# if __has_include(<smmintrin.h>)
+# if __has_include(<smmintrin.h>) && !SIV3D_PLATFORM(WEB)
 # include <smmintrin.h>
 # else
 # include <ThirdParty/simde/x86/sse4.1.h>
 # endif
 
-# if __has_include(<nmmintrin.h>)
+# if __has_include(<nmmintrin.h>) && !SIV3D_PLATFORM(WEB)
 # include <nmmintrin.h>
 # else
 # include <ThirdParty/simde/x86/sse4.2.h>
