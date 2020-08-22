ff4df1c77142e438ea6d0b77b05dee5330a5d07f
nokotan <kamenokonokotan@gmail.com>
2020-08-23 01:21:28 +0900

    [Web] Update Test app

--- Web/App/Main.cpp
+++ Web/App/Main.cpp
@@ -1,22 +1,10 @@
 
 # include <Siv3D.hpp>
-# include <emscripten.h>
+SIV3D_SET(EngineOption::Renderer::Headless) // 非グラフィックモード
 
 void Siv3DTest();
 
-void MainLoop()
-{
-	System::Update();
-
-	for (auto i : step(20))
-	{
-		Rect(Cursor::Pos().movedBy(0 + i * 20, 0), 20, 400)
-			.draw(HSV(i * 10, 0.5, 0.9));
-	}
-}
-
 void Main()
-{	
-	// Siv3DTest();
-	emscripten_set_main_loop(&MainLoop, 0, 1);
+{
+	Siv3DTest();
 }
