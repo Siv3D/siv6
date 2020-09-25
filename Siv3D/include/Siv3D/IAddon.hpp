ad19cb6e14a72b16981fa51aecb4fef45ccf60d7
Ryo Suzuki <reputeless+github@gmail.com>
2020-09-26 00:05:41 +0900

    [共通] Addon 準備

---
+++ Siv3D/include/Siv3D/IAddon.hpp
@@ -0,0 +1,29 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2020 Ryo Suzuki
+//	Copyright (c) 2016-2020 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# pragma once
+# include "Common.hpp"
+
+namespace s3d
+{
+	class IAddon
+	{
+	public:
+
+		virtual ~IAddon() = default;
+
+		virtual bool init() = 0;
+
+		virtual bool update() = 0;
+
+		virtual void draw() const {}
+	};
+}
