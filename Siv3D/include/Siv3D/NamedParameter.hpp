cd5058aabca6c68ba9589c8aac20b6687311f236
Ryo Suzuki <reputeless+github@gmail.com>
2020-10-25 14:20:30 +0900

    [共通] revert NamedParameter

--- Siv3D/include/Siv3D/NamedParameter.hpp
+++ Siv3D/include/Siv3D/NamedParameter.hpp
@@ -15,98 +15,117 @@
 # include <memory>
 # include <utility>
 
+# pragma once
+# include <type_traits>
+# include <functional>
+# include <tuple>
+
 namespace s3d
 {
-	namespace detail
-	{
-		template <class Tag, class... Args>
-		struct NamedParameterInternal
-		{
-			std::tuple<Args...> tuple;
-		};
-	}
-
 	template <class Tag, class Type>
 	class NamedParameter
 	{
-	public:
-
-		static_assert(std::is_object_v<Type>
-			&& std::is_destructible_v<Type>
-			&& not std::is_array_v<Type>);
-
-		using value_type = Type;
-		using reference = value_type&;
-		using const_reference = const value_type&;
-		using pointer = value_type*;
-		using const_pointer = const value_type*;
-
-		template <class U, std::enable_if_t<std::is_constructible_v<value_type, U>>* = nullptr>
-		constexpr NamedParameter(const NamedParameter<Tag, U>& holder) noexcept(std::is_nothrow_constructible_v<value_type, U>)
-			: m_value(holder.value()) {}
-
-		template <class... Args>
-		constexpr NamedParameter(const detail::NamedParameterInternal<Tag, Args...>& parameters)
-			: m_value(std::make_from_tuple<value_type>(parameters.tuple)) {}
-
-		[[nodiscard]]
-		constexpr pointer operator ->() noexcept
-		{
-			return std::addressof(m_value);
-		}
-
-		[[nodiscard]]
-		constexpr const_pointer operator ->() const noexcept
-		{
-			return std::addressof(m_value);
-		}
-
-		[[nodiscard]]
-		constexpr reference operator *() noexcept
-		{
-			return m_value;
-		}
-
-		[[nodiscard]]
-		constexpr const_reference operator *() const noexcept
-		{
-			return m_value;
-		}
-
-		[[nodiscard]]
-		constexpr reference value() noexcept
-		{
-			return m_value;
-		}
-
-		[[nodiscard]]
-		constexpr const_reference value() const noexcept
-		{
-			return m_value;
-		}
-
 	private:
 
-		value_type m_value;
+		Type m_value;
+
+	public:
+
+		NamedParameter() = default;
+
+		constexpr NamedParameter(const Type& value)
+			: m_value(value) {}
+
+		template <class U, class V = Type, std::enable_if_t<std::is_convertible_v<U, V>>* = nullptr>
+		constexpr NamedParameter(const NamedParameter<Tag, U>& other)
+			: m_value(other.value()) {}
+
+		template <class... Args, class V = Type, std::enable_if_t<std::is_constructible_v<V, Args...>>* = nullptr>
+		constexpr NamedParameter(const NamedParameter<Tag, std::tuple<Args...>>& tuple)
+			: m_value(std::make_from_tuple<Type>(tuple.value())) {}
+
+		constexpr const Type* operator ->() const noexcept
+		{
+			return std::addressof(m_value);
+		}
+
+		[[nodiscard]]
+		constexpr const Type& operator *() const noexcept
+		{
+			return m_value;
+		}
+
+		[[nodiscard]]
+		constexpr const Type& value() const noexcept
+		{
+			return m_value;
+		}
+	};
+
+	template <class Tag, class Type>
+	class NamedParameter<Tag, Type&>
+	{
+	private:
+
+		Type* m_ref = nullptr;
+
+	public:
+
+		NamedParameter() = default;
+
+		constexpr NamedParameter(Type& value) noexcept
+			: m_ref(std::addressof(value)) {}
+
+		constexpr Type* operator ->() const noexcept
+		{
+			return m_ref;
+		}
+
+		[[nodiscard]]
+		constexpr Type& operator *() const noexcept
+		{
+			return *m_ref;
+		}
+
+		[[nodiscard]]
+		constexpr Type& value() const noexcept
+		{
+			return *m_ref;
+		}
 	};
 
 	template <class Tag>
 	struct NamedParameterHelper
 	{
 		template <class Type>
+		using named_argument_type = NamedParameter<Tag, Type>;
+
+		template <class Type>
 		[[nodiscard]]
-		constexpr auto operator =(Type&& value) const noexcept
+		constexpr NamedParameter<Tag, std::decay_t<Type>> operator =(Type&& value) const
 		{
-			return detail::NamedParameterInternal<Tag, Type>
-				{ std::forward_as_tuple(std::forward<Type>(value)) };
+			return NamedParameter<Tag, std::decay_t<Type>>(std::forward<Type>(value));
 		}
 
 		template <class... Args>
 		[[nodiscard]]
-		constexpr auto operator ()(Args&&... args) const noexcept
+		constexpr NamedParameter<Tag, std::tuple<std::decay_t<Args >...>> operator() (Args&&... args) const
 		{
-			return detail::NamedParameterInternal<Tag, Args...>
-				{ std::forward_as_tuple(std::forward<Args>(args)...) };
+			return NamedParameter<Tag, std::tuple<std::decay_t<Args >...>>(std::make_tuple(std::forward<Args>(args)...));
+		}
+
+		template <class Type>
+		[[nodiscard]]
+		constexpr NamedParameter<Tag, Type&> operator =(std::reference_wrapper<Type> value) const
+		{
+			return NamedParameter<Tag, Type&>(value.get());
+		}
+
+		template <class Type>
+		[[nodiscard]]
+		constexpr NamedParameter<Tag, Type&> operator()(std::reference_wrapper<Type> value) const
+		{
+			return NamedParameter<Tag, Type&>(value.get());
 		}
 	};
 }
